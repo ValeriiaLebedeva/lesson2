32cdb1e934c59e1721b3dfc9b8cd26c2fcec8c6f
Valeriya <lerikalebedeva4@gmail.com>
2021-11-04 16:42:53 +0300

    add 1.png file, update Tests.java file

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,10 +1,17 @@
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.selector.ByText;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.enabled;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selectors.byClassName;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 
 public class Tests {
 
@@ -15,19 +22,55 @@ public class Tests {
 
     @Test
     void fillFormTest() {
-        open("https://demoqa.com/text-box");
-        $("[id=userName]").setValue("Some name");
+        open("https://demoqa.com/automation-practice-form");
+        $("[id=firstName]").setValue("Name");
+
+        $("[id=lastName]").setValue("Surname");
+
         $("#userEmail").setValue("aaa@aa.aa");
+
+        $("#genterWrapper").$(byText("Male")).click();
+
+        $("[id=userNumber]").setValue("1234567890");
+
+        $("#dateOfBirthInput").click();
+        $(byClassName("react-datepicker__month-select")).click();
+        $(byText("April")).click();
+        $(byClassName("react-datepicker__year-select")).selectOptionByValue("1996");
+        $$(".react-datepicker__day").find(Condition.text("15")).click();
+
+        $("#subjectsContainer").click();
+        $("#subjectsInput").setValue("a");
+        $(byText("Maths")).click();
+
+        $(byText("Sports")).click();
+
+        $("#uploadPicture").uploadFile(new File("src/test/resources/img/1.png"));
+        $("#uploadPicture").uploadFromClasspath("img/1.png");
+
         $("#currentAddress").setValue("Some address");
-        $("#permanentAddress").setValue("Another address");
+
+        $("[id=state]").scrollTo().click();
+        $(byText("NCR")).click();
+
+        $("[id=city]").click();
+        $(byText("Delhi")).click();
+
         $("#submit").click();
 
-        $("#output #name").shouldHave(text("Some name"));
-        $("#output").$("#email").shouldHave(text("aaa@aa.aa"));
-        $("#output").$("#currentAddress").shouldHave(text("Some address"));
-        $("#output").$("#permanentAddress").shouldHave(text("Another address"));
 
-//        $("#output").shouldHave(text("Some name"), text("aaa@aa.aa"),
-//                text("Some address"), text("Another address"));
+        $(byClassName("modal-content")).shouldBe(enabled);
+        $(byClassName("modal-content")).shouldHave(text("Name"));
+        $(byClassName("modal-content")).shouldHave(text("Surname"));
+        $(byClassName("modal-content")).shouldHave(text("aaa@aa.aa"));
+        $(byClassName("modal-content")).shouldHave(text("Male"));
+        $(byClassName("modal-content")).shouldHave(text("Maths"));
+        $(byClassName("modal-content")).shouldHave(text("Sports"));
+        $(byClassName("modal-content")).shouldHave(text("1234567890"));
+        $(byClassName("modal-content")).shouldHave(text("Some address"));
+        $(byClassName("modal-content")).shouldHave(text("15 April,1996"));
+        $(byClassName("modal-content")).shouldHave(text("NCR"));
+        $(byClassName("modal-content")).shouldHave(text("Delhi"));
+
     }
 }
